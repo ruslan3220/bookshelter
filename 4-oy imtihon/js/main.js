699c876e2e1de2888b15ea426a2c1ed8a4967157
ruslan3220 <arifjanovruslan46@gmail.com>
2022-04-19 02:26:53 +0500

    new add js

--- 4-oy imtihon/js/main.js
+++ 4-oy imtihon/js/main.js
@@ -1,95 +1,146 @@
-    let elWrapper = document.querySelector(".wrapper")
-    let elBody = document.querySelector("#body")
-    let elModalWrapper = document.querySelector("#wrapper_modal");
-    let elFrom = document.querySelector("#form")
-    let elInput = document.querySelector("#search_input")
-    let elReadLink = document.querySelector("#read_link")
-    let elMoreBtn = document.querySelector("#more_btn")
-    let elNewBook = document.querySelector(".new_books")
-    let elStarBtn = document.querySelector(".star_btn")
-    let elCount = document.querySelector("#count-books")
-    let elTemplate = document.querySelector("#template").content;
-    let elModalTemplate = document.querySelector("#templateModal").content;
+let elWrapper = document.querySelector(".wrapper")
+let elBody = document.querySelector("#body")
+let elModalWrapper = document.querySelector("#wrapper_modal");
+let elFrom = document.querySelector("#form")
+let elInput = document.querySelector("#search_input")
+let elReadLink = document.querySelector("#read_link")
+let elMoreBtn = document.querySelector("#more_btn")
+let elMoreForm = document.querySelector(".more_form")
+let elNewBook = document.querySelector(".new_books")
+let elStarBtn = document.querySelector(".star_btn")
+let elCount = document.querySelector("#count-books")
+let elTemplate = document.querySelector("#template").content;
+let elNewTemplate = document.querySelector("#newTemplate").content;
+let elModalTemplate = document.querySelector("#templateModal").content;
 
 
 
-    elStarBtn.addEventListener("click", (evt) => {
-        evt.preventDefault()
-        elBody.classList.toggle("dark_body")
-    })
+elStarBtn.addEventListener("click", (evt) => {
+    evt.preventDefault()
+    elBody.classList.toggle("dark_body")
+})
 
-    function renderBooks(booksArray, wrapper) {
+function renderBooks(booksArray, wrapper) {
 
-        let elFragment = document.createDocumentFragment()
-    booksArray.items.forEach(item => {
+    let elFragment = document.createDocumentFragment()
+booksArray.items.forEach(item => {
 
-      let newTemplate = elTemplate.cloneNode(true)
-      let imageLink = `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api`
-      newTemplate.querySelector(".card-img-top").src = imageLink
-      newTemplate.querySelector(".card-title").textContent = item.volumeInfo.title
-      newTemplate.querySelector(".card-author").textContent = item.volumeInfo.authors
-      newTemplate.querySelector(".card-year").textContent = item .volumeInfo.publishedDate
-      newTemplate.querySelector(".bookmark-btn").dataset.bookId = item.id
-      newTemplate.querySelector(".card-link").href = `http://books.google.co.uz/books?id=${item.id}&pg=PA700&dq=phyton&hl=&cd=3&source=gbs_api`
-      elFragment.appendChild(newTemplate)
-    });
-    wrapper.appendChild(elFragment)
-    elCount.textContent = booksArray.items.length
+  let newTemplate = elTemplate.cloneNode(true)
+  let imageLink = `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api`
+  newTemplate.querySelector(".card-img-top").src = imageLink
+  newTemplate.querySelector(".card-title").textContent = item.volumeInfo.title
+  newTemplate.querySelector(".card-author").textContent = item.volumeInfo.authors
+  newTemplate.querySelector(".card-year").textContent = item .volumeInfo.publishedDate
+  newTemplate.querySelector(".bookmark-btn").dataset.bookId = item.id
+  newTemplate.querySelector(".card-link").href = `http://books.google.co.uz/books?id=${item.id}&pg=PA700&dq=phyton&hl=&cd=3&source=gbs_api`
+  elFragment.appendChild(newTemplate)
+});
+wrapper.appendChild(elFragment)
+elCount.textContent = booksArray.items.length
 }
 
-(async function () {
-    let responce = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=javascript`
-        );
-        let data = await responce.json();
-        renderBooks(data, elWrapper)
-    })();
+elFrom.addEventListener("submit", (evt) =>{
+evt.preventDefault()
+let inputValue = elInput.value;
+if (inputValue) {
+       (async function () {
+           let responce = await fetch(
+               `https://www.googleapis.com/books/v1/volumes?q=${inputValue}`
+               );
+               let data = await responce.json();
+               renderBooks(data, elWrapper)
+           })();
 
-    // MORE INFO
+   }
+})
 
-    function renderModal(modalArray, modalWrapper) {
-        let elFragmentModal = document.createDocumentFragment() // O'rab turuvchi
-        modalArray.items.forEach(item => {
-            let newTemplate = elModalTemplate.cloneNode(true) // Ko'piya qilish uchun
-            let imageLink = `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api`
-            newTemplate.querySelector(".card-img-top").src = imageLink
-            newTemplate.querySelector(".card-description").textContent = item.volumeInfo.description
-            newTemplate.querySelector(".card-author").textContent = item.volumeInfo.authors
-            newTemplate.querySelector(".card-year").textContent = item .volumeInfo.publishedDate
-            newTemplate.querySelector(".card-type").textContent = item .volumeInfo.printType
-            newTemplate.querySelector(".card-categories").textContent = item .volumeInfo.categories
-            newTemplate.querySelector(".card-cound").textContent = item .volumeInfo.pageCount
-            newTemplate.querySelector(".card-link").href = `http://books.google.co.uz/books?id=${item.id}&pg=PA700&dq=phyton&hl=&cd=3&source=gbs_api`
-            elFragmentModal.appendChild(newTemplate)
-             });
-           modalWrapper.appendChild(elFragmentModal)
-           console.log(modalWrapper);
+// MORE INFO
+
+function renderModal(modalArray, modalWrapper) {
+    let elFragmentModal = document.createDocumentFragment() // O'rab turuvchi
+    modalArray.items.forEach(item => {
+        let newTemplate = elModalTemplate.cloneNode(true) // Ko'piya qilish uchun
+        let imageLink = `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api`
+        newTemplate.querySelector(".card-img-top").src = imageLink
+        newTemplate.querySelector(".card-description").textContent = item.volumeInfo.description
+        newTemplate.querySelector(".card-author").textContent = item.volumeInfo.authors
+        newTemplate.querySelector(".card-year").textContent = item .volumeInfo.publishedDate
+        newTemplate.querySelector(".card-type").textContent = item .volumeInfo.printType
+        newTemplate.querySelector(".card-categories").textContent = item .volumeInfo.categories
+        newTemplate.querySelector(".card-cound").textContent = item .volumeInfo.pageCount
+        newTemplate.querySelector(".card-link").href = `http://books.google.co.uz/books?id=${item.id}&pg=PA700&dq=phyton&hl=&cd=3&source=gbs_api`
+        elFragmentModal.appendChild(newTemplate)
+         });
+       modalWrapper.appendChild(elFragmentModal)
+       console.log(modalWrapper);
 }
+
+// elMoreBtn.addEventListener("click", (evt) =>{
+//     evt.preventDefault()
+//     let inputValue = elInput.value;
+//     console.log(inputValue);
+//     if (!inputValue) {
+//            (async function () {
+//                let responce = await fetch(
+//                    `https://www.googleapis.com/books/v1/volumes?q=${inputValue}`
+//                    );
+//                    let data = await responce.json();
+//                    renderBooks(data, elWrapper)
+//                })();
+
+//        }
+//     })
 ;(async function () {
-    let responce = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=javascript`
-        );
-        let data = await responce.json();
-        renderModal(data, elModalWrapper)
-    })();
+let responce = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=javascript`
+    );
+    let data = await responce.json();
+    renderModal(data, elModalWrapper)
+})();
+
+// Order by newEst
+
+function sortNewBooks(booksArray, wrapper) {
+
+let elFragment = document.createDocumentFragment()
+booksArray.items.forEach(item => {
+
+let newTemplate = elNewTemplate.cloneNode(true)
+let imageLink = `http://books.google.com/books/content?id=${item.id}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api`
+newTemplate.querySelector(".card-img-top").src = imageLink
+newTemplate.querySelector(".card-title").textContent = item.volumeInfo.title
+newTemplate.querySelector(".card-author").textContent = item.volumeInfo.authors
+newTemplate.querySelector(".card-year").textContent = item .volumeInfo.publishedDate
+newTemplate.querySelector(".bookmark-btn").dataset.bookId = item.id
+newTemplate.querySelector(".card-link").href = `http://books.google.co.uz/books?id=${item.id}&pg=PA700&dq=phyton&hl=&cd=3&source=gbs_api`
+elFragment.appendChild(newTemplate)
+});
+wrapper.appendChild(elFragment)
+elCount.textContent = booksArray.items.length
+}
 
 
-    elNewBook.addEventListener("submit", () =>{
-        (async function () {
-            let responce = await fetch(
-                `https://www.googleapis.com/books/v1/volumes?q=javascript&orderBy=newest`
-                );
-                let data = await responce.json();
-                renderBooks(data, elWrapper)
-            })();
-    })
+elNewBook.addEventListener("click", (evt) =>{
 
-    // bookmark
-    let bookmarkedList = []
-    elWrapper.addEventListener("click", (evt) =>{
-        let booksId = evt.target.dataset.bookId
-        bookmarkedList
-        let foundBook = renderModal.filter(function (item) {
-            return item.id == booksId
-        })
-    })
+let foundUser = evt.target.dataset.bookId;
+console.log(foundUser);
+if(foundUser){
+    (async function () {
+        let responce = await fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=javascript&orderBy=newest`
+            );
+            let data = await responce.json();
+            sortNewBooks(data, elWrapper)
+        })();
+}
+})
+
+// bookmark
+// let bookmarkedList = []
+// elWrapper.addEventListener("click", (evt) =>{
+//     let booksId = evt.target.dataset.bookId
+//     bookmarkedList
+//     let foundBook = renderModal.filter(function (item) {
+//         return item.id == booksId
+//     })
+// })
